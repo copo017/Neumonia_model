770abd35d2bc6783a794e25dff32b8acde38ac1d
copo <sebastianorellana017@gmail.com>
2024-11-04 02:54:59 -0300

    up7

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
 import UploadImage from "./UploadImage";
 
-{/*import UploadImage from "F:/Users/Documents/Program Class/solo Ciencia de Datos/neumonia_modelo/src/UploadImage";*/}
-
 function App() {
     return (
         <div className="App">
             <header>
                 <h1>Predicción de Neumonía en Radiografías</h1>
-                <UploadImage /> {/* Aquí se usa el componente */}
+                <UploadImage />
             </header>
         </div>
     );
